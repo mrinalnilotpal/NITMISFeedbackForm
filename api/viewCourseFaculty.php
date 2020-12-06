437f463f03238a9be7bd76763ca61d9f39d0af29
hari-19 <hari19@outlook.in>
2020-12-06 18:27:57 +0530

    Updated viewCourseFaculty

--- api/viewCourseFaculty.php
+++ api/viewCourseFaculty.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
     {
         $conn = dbConnect();
         $data->roll_no = strtoupper($data->roll_no); 
-        $query = "SELECT A.COURSE_ID, B.COURSE_NAME, D.FAC_NAME FROM Course_Reg A, Course B, Course_Faculty C, Faculty D WHERE ROLL_NO='". $data->roll_no."' AND A.COURSE_ID=B.COURSE_ID AND A.COURSE_ID=C.COURSE_ID AND C.FAC_ID=D.FAC_ID";
+        $query = "SELECT A.COURSE_ID, D.FAC_ID, B.COURSE_NAME, D.FAC_NAME FROM Course_Reg A, Course B, Course_Faculty C, Faculty D WHERE ROLL_NO='". $data->roll_no."' AND A.COURSE_ID=B.COURSE_ID AND A.COURSE_ID=C.COURSE_ID AND C.FAC_ID=D.FAC_ID";
         $result = $conn->query($query);
 
 
@@ -18,9 +18,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
         {   
             while($row = $result->fetch_assoc()) 
             {        $course_id = $row["COURSE_ID"];
+                   $fac_id = $row["FAC_ID"];
                      $course_name = $row["COURSE_NAME"];
                      $fac_name = $row["FAC_NAME"];
-            	     $course_array[]=array("course_id"=> $course_id, "course_name"=>$course_name, "fac_name"=>$fac_name);
+            	     $course_array[]=array("course_id"=> $course_id, "course_name"=>$course_name, "fac_id" => $fac_id , "fac_name"=>$fac_name);
 
             }
             echo json_response(200, json_encode($course_array));
