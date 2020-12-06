a335dd40e00e22014b1e4a197b24ec9fd79608fa
Vishnuprasadh <visprasadhkumaravelu@gmail.com>
2020-12-06 14:40:00 +0530

    Init login

--- ui/feedbackForm/src/app/app.module.ts
+++ ui/feedbackForm/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormPageComponent } from './form-page/form-page.component';
 import { SideHeaderComponent } from './side-header/side-header.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormPageComponent,
-    SideHeaderComponent
+    SideHeaderComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
