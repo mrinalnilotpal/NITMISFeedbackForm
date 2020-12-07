d28c3999f61eac3925fe33991b40deff4c54b70f
hari-19 <hari19@outlook.in>
2020-12-07 23:43:39 +0530

    WIP Form

--- ui/feedbackForm/src/app/form-field/form-field.component.ts
+++ ui/feedbackForm/src/app/form-field/form-field.component.ts
@@ -8,7 +8,7 @@ import { Component, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, Sim
 })
 export class FormFieldComponent implements OnInit, DoCheck {
   @Input() question = "Effectiveness of delivery of lectures and communication";
-  @Input() slNo = "1";
+  @Input() slNo = 1;
   currentRate = 0;
   oldRate: number;
   @Output() currentRateChange = new EventEmitter<number>();
--- ui/feedbackForm/src/app/form/form.component.ts
+++ ui/feedbackForm/src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.sass']
 })
 export class FormComponent implements OnInit {
-  rating = 0;
+  @Input() rating = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  @Input() subjectType = "theory";
+
+  @Output() emitRating = new EventEmitter<number[]>();
+
+  readonly theoryQn = [
+    'Lectures are well prepared',
+    'Effectiveness of delivery of lectures and communication',
+    'Knowledgable and makes the course interesting',
+    'Encourages discussion, creative/ critical thinking and clears doubts',
+    'Regular & punctual to the class',
+    'Cycle test papers are evaluated and distributed in time',
+    'Transparency and fairness of Evaluation system/ Internal Examinations',
+    'Availability of the Faculty after class hours for guidance',
+    'The prescribed syllabi is entirely completed',
+    'The source material (books etc) for each portion covered in the syllabi is clearly informed'
+  ];
+
+  readonly labQn = [
+    'Laboratories are well organized for experiments',
+    'List of experiments are well defined',
+    'Oppurtunity and facilities provided in the Lab for the use of theoritical knowledge gained in the classroom to learn and practice',
+    'Skills and assistance provided during the Laboratory',
+    'Transparency and fairness of Evaluation system for Laboratory Examinations'
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.subjectType === "lab") {
+      this.rating = [0, 0, 0, 0, 0];
+    }
   }
 }
