c64e77a5eb6e9f919426c45380a9df6161a0a0c7
hari-19 <hari19@outlook.in>
2020-12-09 12:18:42 +0530

    Updated toggle buttons

--- ui/feedbackForm/src/app/faculty-dashboard/faculty-dashboard.component.ts
+++ ui/feedbackForm/src/app/faculty-dashboard/faculty-dashboard.component.ts
@@ -10,10 +10,20 @@ export class FacultyDashboardComponent implements OnInit {
   subject = 'Introduction to Mechatronics';
   faculty = 'Guna M';
   subjectCode = 'ME1001';
-  label = "Summary";
+  commentlabel = "Comments";
+  summarylabel = "Summary";
+  isComments = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick(label: string): void {
+    if (label === this.commentlabel) {
+      this.isComments = true;
+    }
+    else {
+      this.isComments = false;
+    }
+  }
 }
