1265a56e81193c5875731c3a4aecfeb0ca6fe842
NILOTPAL MRINAL <nilotpal.avanti@gmail.com>
2020-12-13 12:27:35 +0530

    Update httpHelper.php

--- api/httpHelper.php
+++ api/httpHelper.php
@@ -2,7 +2,6 @@
 
 function json_response($code = 200, $message = null)
 {
-    header_remove();
     http_response_code($code);
     header('Content-Type: application/json');
     header("Access-Control-Allow-Origin: *");
